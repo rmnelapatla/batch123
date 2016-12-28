5e787c7027c436f1eb7cb374beab9e54ed385ed7
ramana <rama_70@yahoo.com>
2016-12-27 19:08:12 -0800

    rama
    Ads

--- workspace/mytestng/src/mytestng/mysimple.java
+++ workspace/mytestng/src/mytestng/mysimple.java
@@ -4,12 +4,12 @@ public class mysimple {
 	
 	public void mydisplay(){
 		
-<<<<<<< HEAD
+
 		System.out.println("Hello");
 		
-=======
+
 		System.out.println("my test by ramana");
->>>>>>> origin/master
+
 	}
 	
 	public static void main(String ara[]){
